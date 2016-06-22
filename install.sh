e2a8dcfb1977c6f4e2c001180751e70f715b0846
楚广明 <chuguangming@chuguangmingdeMacBook-Pro.local>
2016-06-22 17:30:09 +0800

    加入判断expect命令是否存在的检查语法

--- install.sh
+++ install.sh
@@ -11,11 +11,29 @@ result=$(more $etcissue | grep "${ubuntuissue}")
 #如果内容不为空,那么这就是Ubuntu操作系统了
 	if [[ "$result" != ""  ]]
 	then
-	echo "Ubuntu"
+		#判断expect软件包是否存在,如果不存在,我们就安装
+		ubuntusearchresult=$(which expect |grep "no expect in")
+		if [[ "$ubuntusearchresult" != "" ]]
+		then
+		echo "expect软件不存在,我们需要安装"
+		sudo dpkg -i expect_5.45-5ubuntu1_amd64.deb
+		else
+		echo "expect软件存在"
+		fi
 	else
-	echo "Centos"
-	fi
+		#centos的安装包与ubuntu的安装包是不一样的
+		#判断expect软件包是否存在,如果不存在,我们就安装
+		centossearchresult=$(which expect |grep "no expect in")
+		if [[ "$centossearchresult" != "" ]]
+		then
+		echo "expect软件不存在,我们需要安装"
+		rpm -ivh tcl-8.5.7-6.el6.x86_64.rpm
+		rpm -ivh expect-5.44.1.15-5.el6_4.x86_64.rpm
+		else
+		echo "expect软件存在"
+		fi
 
+	fi
 else
 echo "此操作系统不适用本脚本!"
 fi
